894d3693ab0b691b228e2410f25f31659d19ae51
Aliaksandr Hapankou <akkforrs@gmail.com>
2022-11-20 12:24:25 +0200

    feat: add solution task1

---
+++ src/components/countersList.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+    const initialState = [
+        {id:0, value:0, name:"Ненужная вещь"}, 
+        {id:1, value:4, name:"Ложка"}, 
+        {id:2, value:0, name:"Вилка"},
+        {id:3, value:0, name:"Тарелка"},
+        {id:4, value:0, name:"Набор минималиста"},
+    ];    
+    const [counters, setCounters] = useState(initialState);
+    const handleDelete = (id) =>{       
+        const newCounters = counters.filter((c) => c.id !== id);       
+        setCounters(newCounters);
+    };
+    const handleReset = () => {
+        setCounters(initialState);
+        console.log('handle reset');
+    };
+    const handleIncrement = (event,id) => {        
+        const newCounter = counters.map((counter) => {                        
+            if (counter.id === id) {              
+              counter.value = counter.value + 1;
+            }
+            return counter
+        })
+        setCounters(newCounter);      
+    };
+    const handleDecrement = (event,id) => {
+        const newCounter = counters.map((counter) => {                        
+            if (counter.id === id) {              
+              counter.value = counter.value - 1;
+            }
+            return counter
+        })
+        setCounters(newCounter); 
+        
+    }; 
+    return ( 
+    <>
+        {counters.map((count) => (
+          <Counter 
+                key={count.id}                
+                onDelete={handleDelete}
+                {...count}
+                onIncrement={(event) => handleIncrement(event, count.id)}
+                onDecrement={(event) => handleDecrement(event, count.id)}
+            />            
+        ))} 
+        <button 
+        className="btn btn-primary btn-sm m-2" 
+        onClick={handleReset}>Сброс
+        </button>         
+    </> 
+    );   
+};
+
+export default CountersList;
